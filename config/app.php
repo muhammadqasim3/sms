b8efdd757ba92d7da212c98b78a6e7b0ee178fd0
Muhammad Qasim <muhammad.qasim@nexuscorp-ltd.com>
2021-06-08 16:34:25 +0500

    InfyOm added

--- config/app.php
+++ config/app.php
@@ -225,8 +225,10 @@ return [
         'Str' => Illuminate\Support\Str::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-
+        'View' => Illuminate\Support\Facades\View::class, 
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
+        'Flash'     => Laracasts\Flash\Flash::class,
     ],
 
 ];
