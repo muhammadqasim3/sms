f441c5031f1ad62f72d62bef5530bbb20db74efc
Muhammad Qasim <muhammad.qasim@nexuscorp-ltd.com>
2021-06-14 18:57:15 +0500

    frontend

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -155,11 +155,12 @@
         <div class="one_quarter first"> 
           <!-- ################################################################################################ -->
           <ul class="nospace">
-            <li class="btmspace-15"><a href="#"><em class="heading">Prospective Students</em> <img class="borderedbox" src="images/demo/220x95.gif" alt=""></a></li>
-            <li class="btmspace-15"><a href="#"><em class="heading">Current Students</em> <img class="borderedbox" src="images/demo/220x95.gif" alt=""></a></li>
-            <li class="btmspace-15"><a href="#"><em class="heading">International Students</em> <img class="borderedbox" src="images/demo/220x95.gif" alt=""></a></li>
-            <li><a href="#"><em class="heading">Alumni</em> <img class="borderedbox" src="images/demo/220x95.gif" alt=""></a></li>
-          </ul>
+     
+          <li class="btmspace-15"><a href="#"><em class="heading">Prospective Students</em> <img class="borderedbox" src="{{ asset('frontend/images/demo/220x95.gif') }}" alt=""></a></li>
+            <li class="btmspace-15"><a href="#"><em class="heading">Current Students</em> <img class="borderedbox" src="{{ asset('frontend/images/demo/220x95.gif') }}" alt=""></a></li>
+            <li class="btmspace-15"><a href="#"><em class="heading">International Students</em> <img class="borderedbox" src="{{ asset('frontend/images/demo/220x95.gif') }}" alt=""></a></li>
+            <li><a href="#"><em class="heading">Alumni</em> <img class="borderedbox" src="{{ asset('frontend/images/demo/220x95.gif') }}" alt=""></a></li>
+        </ul>
           <!-- ################################################################################################ --> 
         </div>
         <!-- / Left Column --> 
